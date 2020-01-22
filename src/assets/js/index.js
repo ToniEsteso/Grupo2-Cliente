573519078bd2ef3ea0d30da31f451c2e412f693a
Jose Miguel Zomeño Escobar <josemi2018@hotmail.com>
2020-01-22 20:08:24 +0100

    fix:(#47): creadas variables en index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,21 @@
 // let urlCliente = "http://localhost/Grupo2";
-import { urlCliente } from "../../config.js";
-import { urlImagenes } from "../../config.js";
-import { urlServidor } from "../../config.js";
+// import { urlCliente, urlImagenes, urlServidor } from "../../config.js";
+
+// LOCALHOST
+urlServidor = "http://127.0.0.1:8000/api";
+urlCliente = "http://localhost/Grupo2";
+urlImagenes = "http://127.0.0.1:8000";
+
+//SERVIDOR CLASE
+// let urlServidor = "http://172.16.205.54:10320/api";
+// let urlCliente = "http://172.16.205.54:10310";
+// let urlImagenes = "http://172.16.205.54:10320";
+
+// SERVIDOR FRANCIA
+// let urlServidor = "http://www.api.veganfood.pve2.fpmislata.com/public/api";
+// let urlCliente = "http://www.veganfood.fpmislata.pve2.fpmislata.com";
+// let urlImagenes = "http://www.api.veganfood.pve2.fpmislata.com";
+
 class Producto {
   constructor(id, nombre, precio, descripcion, imagen) {
     this.id = id;
