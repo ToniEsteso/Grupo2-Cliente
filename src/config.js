28902f9f7548d620355b632be2a9d22382745e5b
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-01-23 09:43:35 +0100

    fix(#47):puesto para archivo de configuracion

--- src/config.js
+++ src/config.js
@@ -1,19 +1,17 @@
-// let urlServidor, urlCliente, urlImagenes;
-// $(document).ready(function() {
-// LOCALHOST
-//   urlServidor = "http://127.0.0.1:8000/api";
-//   urlCliente = "http://localhost/Grupo2";
-//   urlImagenes = "http://127.0.0.1:8000";
+let urlServidor, urlCliente, urlImagenes;
+LOCALHOST
+  urlServidor = "http://127.0.0.1:8000/api";
+  urlCliente = "http://localhost/Grupo2";
+  urlImagenes = "http://127.0.0.1:8000";
 
-//SERVIDOR CLASE
-// let urlServidor = "http://172.16.205.54:10320/api";
-// let urlCliente = "http://172.16.205.54:10310";
-// let urlImagenes = "http://172.16.205.54:10320";
+// SERVIDOR CLASE
+// urlServidor = "http://172.16.205.54:10320/api";
+// urlCliente = "http://172.16.205.54:10310";
+// urlImagenes = "http://172.16.205.54:10320";
 
 // SERVIDOR FRANCIA
-// let urlServidor = "http://www.api.veganfood.pve2.fpmislata.com/public/api";
-// let urlCliente = "http://www.veganfood.fpmislata.pve2.fpmislata.com";
-// let urlImagenes = "http://www.api.veganfood.pve2.fpmislata.com";
-// });
+// urlServidor = "http://www.api.veganfood.pve2.fpmislata.com/public/api";
+// urlCliente = "http://www.veganfood.fpmislata.pve2.fpmislata.com";
+// urlImagenes = "http://www.api.veganfood.pve2.fpmislata.com";
 
-// export { urlServidor, urlCliente, urlImagenes };
+export { urlServidor, urlCliente, urlImagenes };
