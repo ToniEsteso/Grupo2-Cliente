7745de8800a4d962445d7506c7a6f2e6e9e68f9c
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-01-23 11:40:57 +0100

    fix(#50): desplegar cliente

--- scripts/desplegarCliente.sh
+++ scripts/desplegarCliente.sh
@@ -6,4 +6,4 @@ git clone -b develop https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/fro
 npm run build --prefix ./html/frontend/Grupo2-Cliente/
 mv ./html/frontend/Grupo2-Cliente/dist ./html/frontend/
 rm -rf ./html/frontend/Grupo2-Cliente
-docker container run -d --name veganfood_frontend -v ./html/frontend:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net “nginx-net” httpd:2.4.41
+docker container run -d --name veganfood_frontend -v html/frontend:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
