534709330e56c1134cfcf157b9a40052d13b4b06
Jose Miguel Zomeño Escobar <josemi2018@hotmail.com>
2020-01-23 22:12:01 +0100

    feat(#50):script despliegue servidor

---
+++ scripts/desplegarServidor.sh
@@ -0,0 +1,9 @@
+#!usr/env/bin bash
+docker container stop veganfood_backend
+docker container rm veganfood_backend
+rm -rf ./html/backend/
+mkdir -p ./html/frontend/Grupo2-Servidor
+git clone -b develop https://github.com/ToniEsteso/Grupo2-Servidor.git ./html/frontend/Grupo2-Servidor
+chmod -R 777 ./html/backend/
+sudo docker container run -d --name veganfood_backend -v /opt/veganfood/html/backend:/var/www/html --expose 80 -e VIRTUAL_HOST=www.api.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.api.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
+~ php:7.3-apache
