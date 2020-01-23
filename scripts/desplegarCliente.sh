07fe4b70bc29941e10ae84eee990a7d1939d618a
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-01-23 13:44:57 +0100

    feat(#50): probando despliegue

--- scripts/desplegarCliente.sh
+++ scripts/desplegarCliente.sh
@@ -1,11 +1,13 @@
+#!usr/bin/env/bash
 docker container stop veganfood_frontend
 docker container rm veganfood_frontend
 rm -rf ./html/frontend/
 mkdir -p ./html/frontend/Grupo2-Cliente
 git clone -b develop https://github.com/ToniEsteso/Grupo2-Cliente.git ./html/frontend/Grupo2-Cliente
 chmod -R 777 ./html/frontend/
-npm rebuild node-sass --prefix ./html/frontend/Grupo2-Cliente
+rm -rf ./html/frontend/Grupo2-Cliente/node_modules
+npm install
 npm run build --prefix ./html/frontend/Grupo2-Cliente/
 mv ./html/frontend/Grupo2-Cliente/dist ./html/frontend/
 rm -rf ./html/frontend/Grupo2-Cliente
-docker container run -d --name veganfood_frontend -v html/frontend:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
+docker container run -d --name veganfood_frontend -v /opt/veganfood/html/frontend:/usr/local/apache2/htdocs --expose 80 -e VIRTUAL_HOST=www.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
