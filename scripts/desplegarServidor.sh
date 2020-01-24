aaf709e0ec6f1bbd49b55abb5f2798b1bf7576a3
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-01-24 08:58:44 +0100

    fix(#50): script desplegar servidor

--- scripts/desplegarServidor.sh
+++ scripts/desplegarServidor.sh
@@ -7,3 +7,6 @@ git clone -b develop https://github.com/ToniEsteso/Grupo2-Servidor.git ./html/fr
 chmod -R 777 ./html/backend/
 sudo docker container run -d --name veganfood_backend -v /opt/veganfood/html/backend:/var/www/html --expose 80 -e VIRTUAL_HOST=www.api.veganfood.pve2.fpmislata.com -e LETSENCRYPT_HOST=www.api.veganfood.pve2.fpmislata.com --net "nginx-net" httpd:2.4.41
 ~ php:7.3-apache
+docker exec -it veganfood_backend a2enmod rewrite
+docker exec -it veganfood_backend docker-php-ext-install pdo pdo_mysql
+docker restart veganfood_backend
