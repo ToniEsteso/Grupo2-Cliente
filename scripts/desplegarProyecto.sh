cd823777731dd9dcc9dc93e06640638b76b5e833
JOSE MIGUEL ZOMEÑO ESCOBAR <20499963v@AD.FPMISLATA.COM>
2020-01-29 12:04:41 +0100

    feat(#66): hecho script para desplegar BD y implementado

--- scripts/desplegarProyecto.sh
+++ scripts/desplegarProyecto.sh
@@ -1,3 +1,7 @@
 #!usr/env/bin bash
 sh desplegarServidor.sh
+echo "<---------------Desplegado backend--------------->"
+sh desplegarMysql.sh
+echo "<---------------Desplegada BD--------------->"
 sh desplegarCliente.sh
+echo "<---------------Desplegado frontend--------------->"
