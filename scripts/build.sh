0da6dd027d7617eb3f019e9197b980edafc0a9ef
JoseMiZome <50233950+JoseMiZome@users.noreply.github.com>
2020-02-11 10:16:30 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,6 @@ echo building.....
 rm -rf ./dist
 mkdir ./dist
 
-shopt -s dotglob
-cp -R ./src/* ./dist/
 rsync -a ./src/* ./dist
 
 node-sass ./dist/assets/scss/main.scss > ./dist/assets/css/main.css
